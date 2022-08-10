7906faf6860e52d203619c9254a0aaa287f82500
Worrapon <nine6805@gmail.com>
2022-08-10 14:22:02 +0700

    exercise 3 python 1

---
+++ Python1/exercise2.py
@@ -0,0 +1,16 @@
+print(" *** Wind classification ***")
+wind_speed = float(input("Enter wind speed (km/h) : "))
+classific = ''
+
+if wind_speed >= 209 :
+    classific = 'Super Typhoon'
+elif wind_speed >= 102 :
+    classific = 'Typhoon'
+elif wind_speed >= 56 :
+    classific = 'Tropical Storm'
+elif wind_speed >= 52 : 
+    classific = 'Depression'
+else :
+    classific = 'Breeze'
+
+print("Wind classification is {}.".format(classific))
---
+++ Python1/exercise3.py
@@ -0,0 +1,8 @@
+print(" *** Summation of each digit ***")
+num = int(input("Enter a positive number : "))
+sum = 0
+while(num > 0):
+    sum += int(num % 10)
+    num = num//10
+
+print("Summation of each digit =  {:d}".format(sum))
