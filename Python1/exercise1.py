e016da6328d802e7176532b974d77fc6d677e918
Worrapon <nine6805@gmail.com>
2022-08-10 13:58:40 +0700

    exercise 1

---
+++ Python1/exercise1.py
@@ -0,0 +1,16 @@
+from posixpath import split
+
+
+print("*** Converting hh.mm.ss to seconds ***")
+time_input = input("Enter hh mm ss : ")
+time = [int(x) for x in time_input.split()]
+
+# check if it valid or not
+for i in time :
+    if i >= 60 or i < 0 :
+        print("mm({}) is invalid!".format(i))
+        exit()
+
+second = time[0]*3600 + time[1]*60 + time[2] # hh mm ss
+
+print("{:02d}:{:02d}:{:02d} = {:,} seconds".format(time[0], time[1], time[2], second))
