0c45ff34b205fd9d39505e58dd67a685dfcc91aa
Worrapon <nine6805@gmail.com>
2022-08-16 20:17:41 +0700

    Exercise 2 stack

--- Stack/stack_2.py
+++ Stack/stack_2.py
@@ -1,6 +1,3 @@
-from inspect import ismemberdescriptor
-
-
 class Stack :
     def __init__(self, list = None) :
         if list == None:
@@ -8,9 +5,8 @@ class Stack :
         else :
             self.items = list
 
-
     def isEmpty(self) :
-        return self.item == []
+        return self.items == []
 
     def size(self) :
         return len(self.items)
@@ -21,7 +17,7 @@ class Stack :
 
     def pop(self) :
         if not self.isEmpty() :
-            return "Add = " + self.items.pop() 
+            return "Pop = " + self.items.pop() 
         else :
             return "-1"
 
@@ -30,27 +26,60 @@ class Stack :
             return self.items[-1]
 
     def delete(self, num) :
-        self.items.remove(num)
-        return "Delete = " + num
+        temp = Stack()
+        if self.isEmpty() : print(-1)
+        for item in self.items :
+            if item == num : 
+                temp.push(num) 
+        self.items = [x for x in self.items if x != num]
+        while not temp.isEmpty() :
+            print("Delete = " + temp.peek())
+            temp.pop()
+            
 
     def lessThan_delete(self, num) :
-        if self.isEmpty() : return -1
+        temp = Stack()
+        if self.isEmpty() : print("-1")
+        for item in self.items :
+            if int(item) < int(num) : 
+                temp.push(item) 
+        # print(self.items)
+        self.items = [x for x in self.items if int(x) >= int(num)]
+        while not temp.isEmpty() :
+            print("Delete = {} Because {} is less than {}".format(temp.peek(),temp.peek(), num))
+            temp.pop()
 
     def moreThan_delete(self, num) :
-        if self.isEmpty() : return -1
+        temp = Stack()
+        if self.isEmpty() : print("-1")
+        for item in self.items :
+            if item > num : 
+                temp.push(item) 
+        self.items = [x for x in self.items if x <= num]
+        while not temp.isEmpty() :
+            print("Delete = {} Because {} is more than {}".format(temp.peek(),temp.peek(), num))
+            temp.pop()
         
     def __str__(self) :
-        s = "Value in Stack = " + self.items
+        int_items = list(map(int, self.items))
+        s = "Value in Stack = " + str(int_items)
         return s
 
-
 stack = Stack()
+
 
 list_input = [x for x in input("Enter Input : ").replace(',', ' ').split()]
 
 for i in range(len(list_input)) :
     if list_input[i] == 'A' :
-        stack.push(list_input[i + 1])
-        print("Add = {} and Size = {}".format(list_input[i+1], stack.size()))
+        print(stack.push(list_input[i + 1]))
     elif list_input[i] == 'P' :
         print(stack.pop())
+    elif list_input[i] == 'D' :
+        stack.delete(list_input[i + 1])
+    elif list_input[i] == 'LD' :
+        stack.lessThan_delete(list_input[i+1])
+    elif list_input[i] == 'MD' :
+        stack.moreThan_delete(list_input[i + 1])
+
+print(stack)
