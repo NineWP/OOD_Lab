74da3db8c348ab90af4cbde083b66183b297c403
Worrapon <nine6805@gmail.com>
2022-08-18 14:11:41 +0700

    queue ex 3

--- Atest.py
+++ Atest.py
@@ -1,2 +1,12 @@
-import os
-os.system('ls ./student_data -al')
+# import os
+# os.system('ls ./student_data -al')
+
+
+ch = 'M'
+ 
+# Using chr()+ord()
+# prints P
+x = chr(ord(ch) + 3)
+ 
+print ("The incremented character value is : ",end="")
+print ((ord('a') + 3))
